aef739a0426b417de4d29a05f3afa203424f6b04
craigbarrau <cbarr.aus@gmail.com>
2015-07-04 13:12:31 +0200

    Puppet-lint fixes

--- myst/manifests/config.pp
+++ myst/manifests/config.pp
@@ -1,7 +1,12 @@
+# == Class: myst::config
+#
+# Sets up the MYST_HOME and PATH environment variables for MyST
+#
 class myst::config inherits myst {
-  if $environment_enable == true {
+  if $::environment_enable == true {
     case $::osfamily {
-      'Linux', 'AIX', 'Debian', 'RedHat', 'SuSE', 'FreeBSD', 'Archlinux', 'Gentoo' : {
+      'Linux', 'AIX', 'Debian', 'RedHat', 'SuSE',
+      'FreeBSD', 'Archlinux', 'Gentoo' : {
         file { '/etc/profile.d/myst-env.sh':
           ensure  => present,
           content => template('myst/myst-env.sh.erb'),
@@ -12,15 +17,17 @@ class myst::config inherits myst {
       }
       'Darwin' : {
         file_line { 'update-mac-path_1':
-          line => "export MYST_HOME=${myst_home}",
+          line => "export MYST_HOME=${::myst_home}",
           path => '/etc/profile'
         }
 
         file_line { 'update-mac-path_2':
-          line    => "export PATH=\$PATH:${myst_home}",
+          line    => "export PATH=\$PATH:${::myst_home}",
           path    => '/etc/profile',
           require => File_line[update-mac-path_1]
         }
+      } default: {
+        fail($::unsupported_msg)
       }
     }
   } else {
